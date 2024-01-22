6cba7f3f1c0974344035f3854750195c684e45e1
Apps247 <aprameyaithal@gmail.com>
2024-01-21 16:50:14 -0800

    Added more examples

--- code/main.py
+++ code/main.py
@@ -5,15 +5,16 @@ from maze import Maze
 
 sys.setrecursionlimit(3500000)
 
-# maze = Maze.from_image('../source_images/stock_maze.jpg')
-# maze = Maze.from_image('../source_images/stock_maze_2.webp')
-maze = Maze.from_image('../source_images/OIP.jpeg')
+maze = Maze.from_image('../source_images/city.webp')
 
 # print(end - start)
 
 maze_image = maze.render('../output/render.png')
 
-# maze_solutions = (maze.render_possible_paths((202, 9), (202, 602), '../output/solution.png'))
-# maze_solutions = (maze.render_possible_paths((50, 366), (750, 432), '../output/solution.png'))
-# maze_solutions = (maze.render_possible_paths((3, 66), (347, 249), '../output/solution.png'))
-maze_solutions = (maze.render_possible_paths((9, 83), (285, 225), '../output/solution.png'))
+
+# * Examples
+# maze_solutions = (maze.render_possible_paths((202, 9), (202, 602), '../output/solution.png')) # stock_maze.jpg
+# maze_solutions = (maze.render_possible_paths((50, 366), (750, 432), '../output/solution.png')) # stock_maze_2.webp
+maze_solutions = (maze.render_possible_paths((3, 2), (602, 1283), '../output/solution.png')) # city.webp
+# maze_solutions = (maze.render_possible_paths((3, 66), (347, 249), '../output/solution.png')) # OIP.jpeg
+# maze_solutions = (maze.render_possible_paths((9, 83), (285, 225), '../output/solution.png')) # OIP.jpeg
